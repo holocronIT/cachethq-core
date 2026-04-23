ae6cb60b571a3e6191e52abe9a74e2720e7398be
Holocron <services@holocron.it>
2026-04-23 23:07:34 +0200

    Schedules Fixes

---
+++ src/Events/Schedules/ScheduleDeleted.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Cachet\Events\Schedules;
+
+use Cachet\Concerns\SendsWebhook;
+use Cachet\Enums\WebhookEventEnum;
+use Cachet\Models\Schedule;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ScheduleDeleted
+{
+    use Dispatchable, InteractsWithSockets, SendsWebhook, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Schedule $schedule)
+    {
+        //
+    }
+
+    public function getWebhookPayload(): array
+    {
+        return $this->schedule->toArray();
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+
+    public function getWebhookEventName(): WebhookEventEnum
+    {
+        return WebhookEventEnum::schedule_deleted;
+    }
+}
